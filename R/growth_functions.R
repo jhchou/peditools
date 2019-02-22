9abac0f5e0b4a872bea4e9ed211a647b119c1263
Joseph Chou <joe.nicu@gmail.com>
2019-02-22 10:16:14 -0500

    Refactoring of all chart and measure names

--- R/growth_functions.R
+++ R/growth_functions.R
@@ -1,105 +1,48 @@
-##########################################################################################
-# growth chart functions
+########## LMS growth chart functions ##########
 #
-# lmsData is R object of LMS parameters for a collection of charts, in R/sysdata.rda for the package
-#
-# To get a summary of available charts and their measures:
-# lmsData %>% dplyr::select(Chart, Measure) %>% unique() %>% dplyr::group_by(Chart) %>% dplyr::summarise(Measures = paste(sort(Measure), collapse = ', '))
-#
-# Include age and measure units:
-# lmsData %>%
-#   filter(Chart != 'Fenton2013') %>%
-#   mutate(Measure = paste0(Measure, '(', MeasureUnits,')')) %>%
-#   select(Chart, AgeUnits, Measure) %>%
-#   unique() %>%
-#   group_by(Chart, AgeUnits) %>%
-#   summarise(Measures = paste(sort(Measure), collapse = ', ')) %>%
-#   arrange(Chart)
+# lmsdata (in R/sysdata.rda) is R an object of LMS parameters for a collection of charts
 #
 # To do:
-# [ ] z_lms_to_x -- is this ever used, or just available, to perhaps make growth charts?
-# [ ] DON'T export functions like get_lms, x_lms_to_z
-# [ ] Probably ONLY need to export x_to_z
+# [ ] rename charts
+# [ ] remove default chart to use
+# [ ] consider not exporting all functions, like get_lms, x_lms_to_z
 # [ ] Should include table of charts and their measures in help for x_to_z
-# [ ] Need to rename functions
-# [ ] Need unit tests
-
-
-#' Z and LMS to X function
-#'
-#' Vectorized function to convert Z + LMS parameters to a measurement. If Z, L, M, or S are vectors of different length, will recycle the shorter vectors.
-#' @param Z Z parameter(s)
-#' @param L lambda parameter(s)
-#' @param M mu parameter(s)
-#' @param S sigma parameter(s)
-#' @export
-#' @examples
-#' # z_lms_to_x()
-z_lms_to_x <- function( Z, L, M, S ) { # vectorized function to convert Z + LMS parameters to a measurement
-  # reference explaining LMS parameters: http://www.cdc.gov/growthcharts/percentile_data_files.htm
-
-  # If do unit tests, may want something like the following:
-  #
-  # z_lms_to_x(        -1, 1.41379, 1388.30336, 0.2114) # check individual cases
-  # z_lms_to_x(         0, 1.41379, 1388.30336, 0.2114)
-  # z_lms_to_x(         1, 1.41379, 1388.30336, 0.2114)
-  # z_lms_to_x( c(-1,0,1), 1.41379, 1388.30336, 0.2114) # should recycle properly
-
-  # http://stackoverflow.com/questions/9335099/implementation-of-standard-recycling-rules
-  expand.arguments <- function(...){
-    # usage: expand.arguments(a = 1, b = 2, c = 1:4) --> List of 3: $a 1, 1, 1, 1; $b 2, 2, 2, 2,; $c 1, 2, 3, 4
-    # does NOT enforce shorter lists being length multiples of longer vectors
-    dotList <- list(...)
-    max.length <- max(sapply(dotList, length))
-    lapply(dotList, rep, length = max.length)
-  }
-  temp <- expand.arguments(Z, L, M, S) # expands any of Z, L, M, S to the maximum length of any of the other arguments
-  Z = temp[[1]]
-  L = temp[[2]]
-  M = temp[[3]]
-  S = temp[[4]]
-
-  ifelse(
-    L != 0,
-      M * (1 + L*S*Z) ^ (1/L), # for L != 0
-      M * exp(S*Z)             # for L == 0
-    )
-}
-
+# [ ] rename functions
+# [ ] unit tests
 
 
 #' Get LMS parameters function
 #'
 #' Function to interpolate LMS parameters, given age, gender, chart, and measure
 #' @param age Vector of ages
-#' @param gender Vector of gender, either 'M' or 'F'
+#' @param gender Vector of gender, either 'm' or 'f'
 #' @param chart Which chart to obtain LMS parameters for. Defaults to 'Fenton 2013' premature growth chart
-#' @param measure Which measure for the chart. Defaults to 'Wt'
+#' @param measure Which measure for the chart. Defaults to 'weight'
 #' @export
 #' @examples
-#' get_lms(c(38, 38), c('M', 'F'), chart = 'Fenton2013', measure = 'Wt')
-get_lms <- function( age, gender, chart = 'Fenton2013', measure = 'Wt' ) {
+#' get_lms(c(38, 38), c('m', 'f'), chart = 'fenton_2013', measure = 'weight')
+get_lms <- function( age, gender, chart = 'fenton_2013', measure = 'weight' ) {
   # function to interpolate LMS parameters, given vector of age, vector of gender, unique chart, and unique measure
 
   # MALE
-  lms <- lmsData[ (lmsData$Chart == chart) & (lmsData$Measure == measure) & (lmsData$Gender) == 'M', ]
+  lms <- lmsdata[ (lmsdata$chart == chart) & (lmsdata$measure == measure) & (lmsdata$gender) == 'm', ]
   if (nrow(lms) == 0) { return(NA) } # No chart matched
   # generate functions to interpolate LMS parameters
-  lms <- lms[ order(lms$Age), ] # for safety and robustness, ensure dataframe sorted by Age
-  fxn_l_male <- stats::approxfun( lms$Age, lms$L ) # stats::approxfun: linear interpolation; returns NA if out of range
-  fxn_m_male <- stats::approxfun( lms$Age, lms$M )
-  fxn_s_male <- stats::approxfun( lms$Age, lms$S )
+  lms <- lms[ order(lms$age), ] # for safety and robustness, ensure dataframe sorted by Age
+  fxn_l_male <- stats::approxfun( lms$age, lms$L ) # stats::approxfun: linear interpolation; returns NA if out of range
+  fxn_m_male <- stats::approxfun( lms$age, lms$M )
+  fxn_s_male <- stats::approxfun( lms$age, lms$S )
 
   lms_male <- list(L_male = fxn_l_male(age), M_male = fxn_m_male(age), S_male = fxn_s_male(age))
 
   # FEMALE
-  lms <- lmsData[ (lmsData$Chart == chart) & (lmsData$Measure == measure) & (lmsData$Gender) == 'F', ]
+  lms <- lmsdata[ (lmsdata$chart == chart) & (lmsdata$measure == measure) & (lmsdata$gender) == 'f', ]
   if (nrow(lms) == 0) { return(NA) } # No chart matched
   # generate functions to interpolate LMS parameters
-  lms <- lms[ order(lms$Age), ] # for safety and robustness, ensure dataframe sorted by Age
-  fxn_l_female <- stats::approxfun( lms$Age, lms$L ) # stats::approxfun: linear interpolation; returns NA if out of range
-  fxn_m_female <- stats::approxfun( lms$Age, lms$M )
-  fxn_s_female <- stats::approxfun( lms$Age, lms$S )
+  lms <- lms[ order(lms$age), ] # for safety and robustness, ensure dataframe sorted by Age
+  fxn_l_female <- stats::approxfun( lms$age, lms$L ) # stats::approxfun: linear interpolation; returns NA if out of range
+  fxn_m_female <- stats::approxfun( lms$age, lms$M )
+  fxn_s_female <- stats::approxfun( lms$age, lms$S )
   lms_female <- list(L_female = fxn_l_female(age), M_female = fxn_m_female(age), S_female = fxn_s_female(age))
 
   gender <- tolower(substr(gender, 1, 1))
@@ -174,45 +117,88 @@ x_lms_to_z <- function( x, lms ) {
 #' The following charts are available, with their corresponding measures.
 #' 
 #' \tabular{lll}{
-#'   \strong{Chart} \tab \strong{Age} \tab \strong{Measures}\cr
-#'   CDCbmi \tab months \tab BMI(kg/m2)\cr
-#'   CDCinfant \tab months \tab HC(cm), Len(cm), Wt(kg)\cr
-#'   CDCpedi \tab months \tab Len(cm), Wt(kg)\cr
-#'   CDCskin \tab years \tab subscapular(mm), triceps(mm)\cr
-#'   DownInfant \tab months \tab HC(cm), Len(cm), Wt(kg)\cr
-#'   DownPedi \tab years \tab BMI(kg/m2), HC(cm), Height(cm), Wt(kg)\cr
-#'   Fenton2003 \tab weeks \tab HC(cm), Len(cm), Wt(kg)\cr
-#   Fenton2013 \tab weeks \tab HC(cm), Len(cm), Wt(g)\cr
-#'   Olsen2010 \tab weeks \tab HC(cm), Len(cm), Wt(g)\cr
-#'   OlsenBMI \tab weeks \tab BMI(g/cm2)\cr
-#'   WHOinfant \tab months \tab HC(cm), Len(cm), Wt(kg)\cr
-#'   WHOskin \tab months \tab arm circ(cm), subscapular(mm), triceps(mm)\cr
+#'   \strong{chart} \tab \strong{age_units} \tab \strong{measures} \cr
+#'   addo_2010_skin    \tab years  \tab subscapular(mm), triceps(mm)                     \cr
+#'   cdc_2000_bmi      \tab months \tab bmi(kg/m2)                                       \cr
+#'   cdc_2000_infant   \tab months \tab head_circ(cm), length(cm), weight(kg)            \cr
+#'   cdc_2000_pedi     \tab months \tab height(cm), weight(kg)                           \cr
+#'   fenton_2003       \tab weeks  \tab head_circ(cm), length(cm), weight(kg)            \cr
+#'   fenton_2013       \tab weeks  \tab head_circ(cm), length(cm), weight(g)             \cr
+#'   olsen_2010        \tab weeks  \tab head_circ(cm), length(cm), weight(g)             \cr
+#'   olsen_2015_bmi    \tab weeks  \tab bmi(g/cm2)                                       \cr
+#'   who_2006_infant   \tab months \tab head_circ(cm), length(cm), weight(kg)            \cr
+#'   who_2007_skin_arm \tab months \tab arm_circ(cm), subscapular(mm), triceps(mm)       \cr
+#'   zemel_2015_infant \tab months \tab head_circ(cm), length(cm), weight(kg)            \cr
+#'   zemel_2015_pedi   \tab years  \tab bmi(kg/m2), head_circ(cm), height(cm), weight(kg)
 #' }
-#' 
+#'
 #' @param x Vector of measurements
 #' @param age Vector of age parameters
-#' @param gender Vector of genders, either 'M' or 'F'
+#' @param gender Vector of genders, either 'm' or 'f'
 #' @param chart Uniquely specified chart to obtain LMS parameters for. Defaults to 'Fenton 2013' premature growth chart.
-#' @param measure Uniquely specified measure for the chart. Defaults to 'Wt'.
+#' @param measure Uniquely specified measure for the chart. Defaults to 'weight'.
 #' @export
 #' @examples
-#' # 3, 10, 50, 90, and 97%ile for 30 0/7 week M on Fenton2013
+#' # 3, 10, 50, 90, and 97%ile for 30 0/7 week M on fenton_2013
 #' x_to_z(
 #'     c(774.1259148, 986.5784793, 1388.303356, 1746.218584, 1903.278428),
 #'     rep(30, 5),
-#'     rep('M', 5),
-#'     chart = 'Fenton2013',
-#'     measure = 'Wt'
+#'     rep('m', 5),
+#'     chart = 'fenton_2013',
+#'     measure = 'weight'
 #'   )
 #' round( pnorm( x_to_z(
 #'     c(774.1259148, 986.5784793, 1388.303356, 1746.218584, 1903.278428),
 #'     rep(30, 5),
-#'     rep('M', 5),
-#'     chart = 'Fenton2013',
-#'     measure = 'Wt'
+#'     rep('m', 5),
+#'     chart = 'fenton_2013',
+#'     measure = 'weight'
 #'   ) ), 4)
-x_to_z <- function(x, age, gender, chart = 'Fenton2013', measure = 'Wt') {
+x_to_z <- function(x, age, gender, chart = 'fenton_2013', measure = 'weight') {
     # function to take a measurement x, and specify age, gender, chart, and measure, to return a Z score
     z <- x_lms_to_z( x, get_lms( age, gender, chart, measure ) )
     return( z )
+}
+
+
+
+#' Z and LMS to X function
+#'
+#' Vectorized function to convert Z + LMS parameters to a measurement. If Z, L, M, or S are vectors of different length, will recycle the shorter vectors.
+#' @param Z Z parameter(s)
+#' @param L lambda parameter(s)
+#' @param M mu parameter(s)
+#' @param S sigma parameter(s)
+#' @export
+#' @examples
+#' # z_lms_to_x()
+z_lms_to_x <- function( Z, L, M, S ) { # vectorized function to convert Z + LMS parameters to a measurement
+  # reference explaining LMS parameters: http://www.cdc.gov/growthcharts/percentile_data_files.htm
+
+  # If do unit tests, may want something like the following:
+  #
+  # z_lms_to_x(        -1, 1.41379, 1388.30336, 0.2114) # check individual cases
+  # z_lms_to_x(         0, 1.41379, 1388.30336, 0.2114)
+  # z_lms_to_x(         1, 1.41379, 1388.30336, 0.2114)
+  # z_lms_to_x( c(-1,0,1), 1.41379, 1388.30336, 0.2114) # should recycle properly
+
+  # http://stackoverflow.com/questions/9335099/implementation-of-standard-recycling-rules
+  expand.arguments <- function(...){
+    # usage: expand.arguments(a = 1, b = 2, c = 1:4) --> List of 3: $a 1, 1, 1, 1; $b 2, 2, 2, 2,; $c 1, 2, 3, 4
+    # does NOT enforce shorter lists being length multiples of longer vectors
+    dotList <- list(...)
+    max.length <- max(sapply(dotList, length))
+    lapply(dotList, rep, length = max.length)
+  }
+  temp <- expand.arguments(Z, L, M, S) # expands any of Z, L, M, S to the maximum length of any of the other arguments
+  Z = temp[[1]]
+  L = temp[[2]]
+  M = temp[[3]]
+  S = temp[[4]]
+
+  ifelse(
+    L != 0,
+      M * (1 + L*S*Z) ^ (1/L), # for L != 0
+      M * exp(S*Z)             # for L == 0
+    )
 }
--- tests/testthat/test_growth_functions.R
+++ tests/testthat/test_growth_functions.R
@@ -2,29 +2,29 @@ context("LMS Growth functions")
 
 test_that("x_to_z is vectorized", {
 
-  # > lmsData %>% filter(Chart == 'Fenton2013', Age %in% c(37, 38, 39), Measure == 'Wt')
-  #        Chart Age AgeUnits Gender Measure MeasureUnits       L        M       S
-  # 1 Fenton2013  37    weeks      M      Wt            g 0.74907 2946.661 0.15083
-  # 2 Fenton2013  38    weeks      M      Wt            g 0.66489 3155.875 0.14297
-  # 3 Fenton2013  39    weeks      M      Wt            g 0.59196 3360.478 0.13657
-  # 4 Fenton2013  37    weeks      F      Wt            g 0.34943 2835.085 0.16132
-  # 5 Fenton2013  38    weeks      F      Wt            g 0.26790 3049.668 0.15490
-  # 6 Fenton2013  39    weeks      F      Wt            g 0.20947 3239.306 0.15011
-  
+  # > lmsdata %>% filter(chart == 'fenton_2013', age %in% c(37, 38, 39), measure == 'weight')
+  #         chart age age_units gender measure measure_units       L        M       S
+  # 1 fenton_2013  37     weeks      m  weight             g 0.74907 2946.661 0.15083
+  # 2 fenton_2013  38     weeks      m  weight             g 0.66489 3155.875 0.14297
+  # 3 fenton_2013  39     weeks      m  weight             g 0.59196 3360.478 0.13657
+  # 4 fenton_2013  37     weeks      f  weight             g 0.34943 2835.085 0.16132
+  # 5 fenton_2013  38     weeks      f  weight             g 0.26790 3049.668 0.15490
+  # 6 fenton_2013  39     weeks      f  weight             g 0.20947 3239.306 0.15011
+
   # x <- c(2946.661, 3155.875, 3360.478, 2835.085, 3049.668, 3239.306)
   # age <- c(37, 38, 39, 37, 38, 39)
-  # gender <- c('M','M','M','F','F','F')
-  # gender <- c('F','F','F','M','M','M')
-  # gender <- 'F'
+  # gender <- c('m','m','m','f','f','f')
+  # gender <- c('f','f','f','m','m','m')
+  # gender <- 'f'
   #
-  # x_to_z(x, age, gender, chart = 'Fenton2013', measure = 'Wt')
-  # x_to_z(x, age, gender, chart = 'Fenton2013', measure = 'Wt') %>% pnorm() %>% round(3)
+  # x_to_z(x, age, gender, chart = 'fenton_2013', measure = 'Wt')
+  # x_to_z(x, age, gender, chart = 'fenton_2013', measure = 'Wt') %>% pnorm() %>% round(3)
 
   x      <- c(2946.661, 3155.875, 3360.478, 2835.085, 3049.668, 3239.306) # 50%ile
   age    <- c(      37,       38,       39,       37,       38,       39)
-  gender <- c(     'M',      'M',      'M',      'F',      'F',      'F')
-  expect_equal(round(x_to_z(x, age, gender, chart = 'Fenton2013', measure = 'Wt'), 4), rep(0, length(x)))
+  gender <- c(     'm',      'm',      'm',      'f',      'f',      'f')
+  expect_equal(round(x_to_z(x, age, gender, chart = 'fenton_2013', measure = 'weight'), 4), rep(0, length(x)))
 
   x      <- c(2391.340352, 2595.734374, 2793.553717, 2287.54664, 2486.917178, 2661.786987) # 10%ile
-  expect_equal(round(pnorm(x_to_z(x, age, gender, chart = 'Fenton2013', measure = 'Wt')), 4), rep(0.1, length(x)))
+  expect_equal(round(pnorm(x_to_z(x, age, gender, chart = 'fenton_2013', measure = 'weight')), 4), rep(0.1, length(x)))
 })
